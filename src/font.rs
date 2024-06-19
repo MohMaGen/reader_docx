3d40eb989fe085c51c905652f4b68ce9f6bd5a56
MohMaGen <mohmagen@gmail.com>
2024-06-19 07:33:16 +0300

    fix: change rust-fontconfig on font-loader.

--- src/font.rs
+++ src/font.rs
@@ -1,32 +1,20 @@
 use anyhow::Context;
-use fontconfig::Fontconfig;
-use std::{io::Read, sync::OnceLock};
-
-static FC: OnceLock<Fontconfig> = OnceLock::new();
-
-fn init_fc() -> anyhow::Result<Fontconfig> {
-    Fontconfig::new().context("Can't innit fontconfig.")
-}
 
 pub fn find_font<'a>(
     name: &'a str,
     style: Option<&'a str>,
 ) -> anyhow::Result<rusttype::Font<'static>> {
-    let fc = FC.get_or_try_init(init_fc)?;
+    let builder = font_loader::system_fonts::FontPropertyBuilder::new().family(name);
 
-    let font = fc.find(name, style).context(format!(
-        "Can't find font {name} with style {style:?}, or it's alternatives"
-    ))?;
+    let builder = match style {
+        Some("Italic") => builder.italic(),
+        Some("Bold") => builder.bold(),
+        _ => builder,
+    };
 
-    let mut bytes = Vec::new();
-    std::fs::File::open(font.path)
-        .context("Failed to read font's file.")?
-        .read_to_end(&mut bytes)?;
+    let property = builder.build();
+    let (bytes, index) = font_loader::system_fonts::get(&property).context("Failed to get font")?;
 
-    if let Some(index) = font.index {
-        rusttype::Font::try_from_vec_and_index(bytes, index as u32)
-            .context("Failed to load font with index")
-    } else {
-        rusttype::Font::try_from_vec(bytes).context("Failed to load font")
-    }
+    Ok(rusttype::Font::try_from_vec_and_index(bytes, index as u32)
+        .context("Failed to create font")?)
 }
