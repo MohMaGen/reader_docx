ccdf43894ddee32eae4d3d021d36e22bd80c7000
MohMaGen <mohmagen@gmail.com>
2024-08-20 18:13:16 +0300

    fix: attribute parsing.

--- word_xml/examples/check_parse/main.rs
+++ word_xml/examples/check_parse/main.rs
@@ -1,5 +1,5 @@
 fn main() -> anyhow::Result<()> {
-    let data = include_str!("./document.xml");
+    let data = include_str!("./document_formated.xml");
     let document: word_xml::WordXMLDocument = data.parse()?;
 
     println!("{:#?}", document);
